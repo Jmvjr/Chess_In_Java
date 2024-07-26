59422f4955f7f7a6603594ce710c3bcaf1ce0d09
Jmvjr <jose.mauricio.jr@icloud.com>
2024-07-26 15:35:18 -0300

    CheckMate logic

--- src/application/UI.java
+++ src/application/UI.java
@@ -9,7 +9,6 @@ import java.util.Arrays;
 import java.util.InputMismatchException;
 import java.util.List;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 public class UI {
   public static final String ANSI_RESET = "\u001B[0m";
@@ -55,11 +54,16 @@ public class UI {
     printCapturedPieces(captured);
     System.out.println();
     System.out.println("Turn: " + chessMatch.getTurn());
-    System.out.println("Waiting Player: " + chessMatch.getCurrentPlayer());
-    if(chessMatch.getCheck()){
-      System.out.println("CHECK!");
+    if(!chessMatch.getCheckMate()) {
+      System.out.println("Waiting Player: " + chessMatch.getCurrentPlayer());
+      if (chessMatch.getCheck()) {
+        System.out.println("CHECK!");
+      }
     }
-
+    else {
+      System.out.println("CHECKMATE!");
+      System.out.println("Winner: " + chessMatch.getCurrentPlayer());
+    }
   }
 
   public static void printBoard(ChessPiece[][] pieces){
